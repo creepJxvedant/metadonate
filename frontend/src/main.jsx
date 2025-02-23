302b39539550ee0fc2179775ec4e9fe01c102bd4
creepJxvedant <vedantverma303@gmail.com>
2025-02-23 09:17:10 +0530

    "update"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/MetaDonate">
+    <BrowserRouter basename="/metadonate">
       <App />
     </BrowserRouter>
   </StrictMode>
